d967346ed5635c427349b29b8cea6e75126a4a88
جمال عبد الناصر الهويش <gamalalmaqtary6838@gmail.com>
2025-10-19 12:30:55 +0000

    جيمناي 2.5 فلاش

--- src/ai/index.ts
+++ src/ai/index.ts
@@ -1,8 +1,6 @@
+'use server';
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
-import {config} from 'dotenv';
-
-config();
 
 export const ai = genkit({
   plugins: [
@@ -10,5 +8,5 @@ export const ai = genkit({
       apiKey: process.env.GEMINI_API_KEY,
     }),
   ],
-  model: 'googleai/gemini-1.5-flash',
+  model: 'googleai/gemini-2.5-flash-preview',
 });
